47fcb1c02dd37ba5418be0501142c134f8940197
Ronny Senna <158476715+ronnysenna@users.noreply.github.com>
2025-05-15 13:33:16 -0300

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -8,7 +8,7 @@ const signIn = async () => {
 }
 
 export const authClient = createAuthClient({
-  baseURL:
-    process.env.NEXT_PUBLIC_URL ||
-    (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"),
+  baseURL: typeof window !== "undefined"
+    ? window.location.origin
+    : "https://vendamais-front.dgohio.easypanel.host",
 });
